9de34bb5b468f1624851a503aede7c24d7556787
Esuarez23 <Esuarez23@cuc.edu.co>
2024-10-01 13:25:56 -0500

    hasta app.tsx

---
+++ src/app/componentes/canciones/CancionActualizar.tsx
@@ -0,0 +1,8 @@
+export const CancionActualizar = () => {
+  return (
+    <>
+      Cancion Actualizar <br />
+      Canciones...
+    </>
+  );
+};
---
+++ src/app/componentes/contenedor/NoEncontrado.tsx
@@ -0,0 +1,8 @@
+export const NoEncontrado = () => {
+  return (
+    <>
+      Soy El No Encontrado <br />
+      El de los Errores...
+    </>
+  );
+};
